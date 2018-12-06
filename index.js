076fbda93297ff6233dc13e3ffa2c601d68cecd6
Juan Camilo Rodriguez <juancamilo9607@gmail.com>
2018-12-06 11:55:34 -0500

    Manejo de propiedades y evento basico en rect

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ import Media from './src/playlist/components/media';
 
 const app = document.getElementById('app');
 //ReactDOM.render(que voy a renderizar, donde lo hare);
-render(<Media/>, app);
+render(<Media type="video" title="¿Que es responsive Design" author="Camilo Rodriguez" image="./images/covers/responsive.jpg"/>, app);
---
+++ src/playlist/components/media.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './media.css';
+
+class Media extends Component {
+/*     constructor(props){
+        super(props)
+        this.handleClick = this.handleClick.bind(this);
+    } */
+    handleClick = (event)=>{
+        console.log(this.props.image)
+    }
+    render(){
+        const style={
+            container:{
+                color: '#44546b',
+                cursor: 'pointer',
+                width: 260,
+                border: '1px solid red'
+            }
+        }
+        return (
+            <div className="Media" onClick={this.handleClick}>
+                <div className="Media-cover">
+                    <img className="Media-image" src={this.props.image} alt="" width={260} height={160}>
+                    </img>
+                    <h3 className="Media-title">{this.props.title}</h3>
+                    <p className="Media-author">{this.props.author}</p>
+                </div>
+            </div>
+        )
+    }
+}
+
+Media.propTypes = {
+    image: PropTypes.string,
+    title: PropTypes.string.isRequired,
+    author: PropTypes.string,
+    type: PropTypes.oneOf(['video', 'audio']),
+}
+
+export default Media;
